6b4de2e1f07db13b0e146a6e4f48e058ad811ad0
joelondev <joelonjohnson@gmail.com>
2022-11-12 18:58:34 +0000

    Create feature4.c
    addition of numbers in c

---
+++ feature4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2, add;
+    
+
+    printf("Enter First Number : ");
+    scanf("%d", &num1);
+    printf("Enter Second Number : ");
+    scanf("%d", &num2);
+
+    add = num1 + num2;
+    
+    printf("%d", add);
+    return 0;
+    
+}
